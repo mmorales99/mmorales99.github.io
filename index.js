0c8d5efb555fa9cef1aab3b4eaa83ba9e742dda5
mmorales99 <mjmorales.mcv@gmail.com>
2020-06-30 11:37:07 +0200

    EYY Commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function _sendMail(){
     Email.send({
         SecureToken : "a2c8451d-4453-47f5-8fde-0eb743942650",
         To : "mjmorales.mcv+myWeb@gmail.com",
-        From : "mjmorales.mcv+myWeb@gmail.com",
+        From : "mjmorales.mcv@gmail.com",
         Subject : "Nuevo Usuario!",
         Body : "Hola! Soy" + escape(document.getElementById("fnombre")) + " Y te escribo desde " + escape(document.getElementById("fmail")) + " y te dedico estas palabras: " + escape(document.getElementById("msg")),
         }).then(
